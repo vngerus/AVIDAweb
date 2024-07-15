ab73723fc060ff44d2d8c8aa9dfd5c81104479df
angel <angelsmithlgs@gmail.com>
2024-07-15 19:18:40 -0400

    del: hover de hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: React.FC = () => {
   return (
     <div className="relative w-full h-screen">
       <img src={hero} alt="Biodiversidad" className="w-full h-full object-cover" />
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-start p-16 bg-black-100 bg-opacity-50">
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-start p-16">
         <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold max-w-md">
           Conoce la<br />
           biodiversidad<br />

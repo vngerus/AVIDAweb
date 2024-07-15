2cfb705a97cae23077832f08452c70c7cdbfac4e
angel <angelsmithlgs@gmail.com>
2024-07-15 12:55:17 -0400

    build: structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Footer, Hero, Home, Navbar } from "./components"
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Home />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar"
+import Hero from "./Hero"
+import Home from "./Home"
+import About from "./About"
+import Footer from "./Footer"
+import Error404 from "./Error404"
+
+export {
+    Navbar,
+    Hero,
+    Home,
+    About,
+    Footer,
+    Error404,
+}
